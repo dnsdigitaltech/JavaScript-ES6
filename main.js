7c10aa666733817987b4ae3fcc117bafad1d39ee
Davi Bernardo <dnssites@gmail.com>
2019-10-24 16:41:22 -0300

    Trabalhando com classes

--- main.js
+++ main.js
@@ -1,10 +1,44 @@
-alert('teste');
-
-class Test {
-    metodo() {
-
+class List {
+    constructor() {
+        this.data = [];
     }
-    outro() {
-
+    add(data) {
+        this.data.push(data)
+        console.log(this.data);
     }
 }
+
+//extende a class List
+class TodoList extends List {
+    constructor() {
+        //para chamar a o metodo costrutor da classe pai é bom colocar a propriedade chamada super, é exatamente chamar o método construtor da class pai
+        super();
+        //depois podemos criar as demais propriedades
+        this.usuario = 'Davi';
+    }
+    mostraUsuario() {
+        console.log(this.usuario);
+    }
+}
+//coloquei na classs List
+/*class TodoList {
+    constructor() {
+        this.todos = [
+
+        ]
+    }
+
+    addTodo() {
+        this.todos.push('Novo todo');
+        console.log(this.todos);
+    }
+}*/
+
+var MinhaLista = new TodoList();
+
+document.getElementById('novotodo').onclick = function() {
+    MinhaLista.add('Novo todo');
+    //MinhaLista.addTodo();
+}
+
+MinhaLista.mostraUsuario();
