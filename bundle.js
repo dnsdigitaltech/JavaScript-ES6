5e9775bdae4f8c375ecd8f063822d3dc94878af5
Davi Bernardo <dnssites@gmail.com>
2019-10-25 14:12:47 -0300

    trabalhando com arrow functions

--- bundle.js
+++ bundle.js
@@ -1,24 +1,54 @@
 "use strict";
 
-/////////////////Operação em Array///////////
-var arr = [1, 3, 4, 5, 8, 9]; //map-> função que serve pra percorrer o vetor e retornar o alguma informação
+/////////////////Arrow Functions///////////
+//uma das funcionalidades mais usadas do ES6
+var arr = [1, 2, 3, 5, 8, 9]; //const newArr = arr.map(function(item) { sem arrow functios
+//com arrow functions em functions anônimas
+//const newArr = arr.map((item) => {
+//quando recebe apenas um paremetro podemos remover os parentes
+
+/*const newArr = arr.map(item => {
+    return item * 2; //multiplica todos os item do vetor por 2
+});
+console.log(newArr);*/
+//quando o retorno não é varias linhas basta copiar o conteudo e colocar depois da flaxinha 
 
 var newArr = arr.map(function (item) {
-  return item * 2; //multiplica todos os item do vetor por 2
+  return item * 2;
 });
-console.log(newArr); //reduce-> função que pode consumir todo nosso vetor e tranformar em uma unica informação
+console.log(newArr); //podemos criar unções usando constantes, porem não é recomentado
 
-var sum = arr.reduce(function (total, next) {
-  return total + next;
-});
-console.log(sum); //filter-> função que pegar uma informação através de filtro
+var teste = function teste() {
+  return 'teste';
+};
 
-var filter = arr.filter(function (item) {
-  return item % 2 === 0;
-});
-console.log(filter); //find-> função que verifica se existe uma informação e a encontra no array
+console.log(teste()); //retornando um array desta função
 
-var find = arr.find(function (item) {
-  return item === 78;
-});
-console.log(find);
+var teste2 = function teste2() {
+  return [1, 2, 3];
+};
+
+console.log(teste2()); //retornando um objeto desta função
+//não retorna pois a {} e para o corpo da função
+
+var teste3 = function teste3() {
+  name: 'Davi';
+};
+
+console.log(teste3()); //desta maneira funciona
+
+var teste4 = function teste4() {
+  return {
+    name: 'Davi'
+  };
+};
+
+console.log(teste4()); //mais se quizer retornar o bjetos depois da flas coloca ()
+
+var teste5 = function teste5() {
+  return {
+    name: 'Davi Bernardo'
+  };
+};
+
+console.log(teste5());
--- main.js
+++ main.js
@@ -1,29 +1,41 @@
-/////////////////Operação em Array///////////
-const arr = [1, 3, 4, 5, 8, 9];
+/////////////////Arrow Functions///////////
+//uma das funcionalidades mais usadas do ES6
+const arr = [1, 2, 3, 5, 8, 9];
 
-//map-> função que serve pra percorrer o vetor e retornar o alguma informação
-const newArr = arr.map(function(item) {
+//const newArr = arr.map(function(item) { sem arrow functios
+//com arrow functions em functions anônimas
+//const newArr = arr.map((item) => {
+//quando recebe apenas um paremetro podemos remover os parentes
+/*const newArr = arr.map(item => {
     return item * 2; //multiplica todos os item do vetor por 2
 });
+console.log(newArr);*/
+//quando o retorno não é varias linhas basta copiar o conteudo e colocar depois da flaxinha 
+const newArr = arr.map(item => item * 2);
 
 console.log(newArr);
 
-//reduce-> função que pode consumir todo nosso vetor e tranformar em uma unica informação
-const sum = arr.reduce(function(total, next) {
-    return total + next;
-});
+//podemos criar unções usando constantes, porem não é recomentado
+const teste = () => {
+    return 'teste';
+}
 
-console.log(sum);
+console.log(teste());
 
-//filter-> função que pegar uma informação através de filtro
-const filter = arr.filter(function(item) {
-    return item % 2 === 0;
-});
+//retornando um array desta função
+const teste2 = () => [1, 2, 3];
 
-console.log(filter);
+console.log(teste2());
 
-//find-> função que verifica se existe uma informação e a encontra no array
-const find = arr.find(function(item) {
-    return item === 78;
-});
-console.log(find);
+//retornando um objeto desta função
+//não retorna pois a {} e para o corpo da função
+const teste3 = () => { name: 'Davi' };
+console.log(teste3());
+//desta maneira funciona
+const teste4 = () => {
+    return { name: 'Davi' }
+};
+console.log(teste4());
+//mais se quizer retornar o bjetos depois da flas coloca ()
+const teste5 = () => ({ name: 'Davi Bernardo' });
+console.log(teste5());
